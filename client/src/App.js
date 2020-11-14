edd72dba292ae40a694ba06e6b1292f5f657efe3
allanperlee <48000848+allanperlee@users.noreply.github.com>
2020-11-14 14:02:53 -0700

    Experimented with the react components in the App.js file. Instantiated the Abilities contract. Line 42 gives an error: This contract object doesn't have address set yet, please set an address first.

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
-import SimpleStorageContract from "./contracts/SimpleStorage.json";
+import Abilities from "./contracts/Abilities.json";
 import getWeb3 from "./getWeb3";
 
 import "./App.css";
 
 class App extends Component {
-  state = { storageValue: 0, web3: null, accounts: null, contract: null };
+  state = { ancestorName: "", web3: null, accounts: null, contract: null, newValue: "" };
 
   componentDidMount = async () => {
     try {
@@ -17,9 +17,9 @@ class App extends Component {
 
       // Get the contract instance.
       const networkId = await web3.eth.net.getId();
-      const deployedNetwork = SimpleStorageContract.networks[networkId];
+      const deployedNetwork = Abilities.networks[networkId];
       const instance = new web3.eth.Contract(
-        SimpleStorageContract.abi,
+        Abilities.abi,
         deployedNetwork && deployedNetwork.address,
       );
 
@@ -35,17 +35,30 @@ class App extends Component {
     }
   };
 
+  async handleChange(event) {
+    this.setState({newValue: event.target.value});
+  }
+
+  async handleSubmit(event) {
+    event.preventDefault();
+
+    const {accounts, contract} = this.state;
+    await contract._makeRandomAncestor(this.state.newValue, { from: accounts[0] })
+    const response = await contract._makeRandomAncestor();
+    this.setState({ancestorName: response});
+  }
+
   runExample = async () => {
     const { accounts, contract } = this.state;
 
     // Stores a given value, 5 by default.
-    await contract.methods.set(5).send({ from: accounts[0] });
+    await contract.methods._makeRandomAncestor("").send({ from: accounts[0] });
 
     // Get the value from the contract to prove it worked.
-    const response = await contract.methods.get().call();
+    const response = await contract.methods.balanceOf(accounts[0]).call();
 
     // Update state with the result.
-    this.setState({ storageValue: response });
+    this.setState({ ancestorName: response });
   };
 
   render() {
